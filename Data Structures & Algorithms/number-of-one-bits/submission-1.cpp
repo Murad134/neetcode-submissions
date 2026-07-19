237955600395ffdc3f76da1c19b47a2e4d076e4b
MD. MURAD HOSEN MUNNA <200134.cse@student.just.edu.bd>
2026-07-19 21:30:19 +0600

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+   public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            return __builtin_popcount(n);
+        }
+        return cnt;
+    }
+};
